4fb9ff94e259709104b92d563254351cb0fcde08
Amine Sagaama <amine.sagaama@gmail.com>
2018-01-08 22:05:37 +0100

    Show catamorphism using Fix fix point

--- src/main/scala/com/asagaama/rs/exp2/Exp2.scala
+++ src/main/scala/com/asagaama/rs/exp2/Exp2.scala
@@ -1,4 +1,4 @@
-package com.asagaama.rs
+package com.asagaama.rs.exp2
 
 import matryoshka._, Recursive.ops._
 import scalaz._
@@ -10,6 +10,25 @@ case class IntExp[A](v: Int) extends Exp[A]
 
 case class SumExp[A](exp1: A, exp2: A) extends Exp[A]
 
-object Exp2 {
+case class MultiplyExp[A](exp1: A, exp2: A) extends Exp[A]
+
+object Exp2 extends App {
+
+  val exp0 = SumExp[Exp[Unit]](IntExp(1), IntExp(2))
+  val exp1 = MultiplyExp[Exp[Exp[Unit]]](
+    SumExp[Exp[Unit]](
+      IntExp(2),
+      IntExp(2)),
+    IntExp[Exp[Unit]](5)
+  )
+
+  def evaluate(exp: Exp[Int]): Int = exp match {
+    case SumExp(exp1, exp2) => exp1 + exp2
+    case MultiplyExp(exp1, exp2) => exp1 * exp2
+    case IntExp(v) => v
+  }
+
+  println(evaluate(IntExp[Int](10)))
+  // will not compile println(evaluate(exp0))
 
 }
